c513fe188366866122aaec5b19b5a35807f24e01
Saket Rahate <117768400+SaketRahate2808@users.noreply.github.com>
2025-11-04 15:21:55 +0530

    Create Task2.cs
    Geometry Overloading

---
+++ Assignment Number 17/Task2.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Drawing;
+class UserAccessControl
+{
+    static void Geometry(int side  )
+    {
+        int Area = side * side;
+        Console.WriteLine($"The area of the sqaure is {Area}");
+    }
+
+    static void Geometry(double length , double breadth)
+    { 
+        double Area = length * breadth;
+        Console.WriteLine($"The area of the sqaure is {Area}");
+
+    }
+
+    static void Geometry(float radius)
+    {
+        float Area = (float)(Math.PI * radius * radius);
+        Console.WriteLine($"The area of the sqaure is {Area}");
+
+    }
+
+
+    static void Main()
+    {
+        Console.WriteLine("Enter the side for square ");
+       int s = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the length of the rectangle ");
+        double l = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the breadth of the rectangle ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the radius of the circle ");
+        float r = float.Parse(Console.ReadLine());
+        Geometry(s);
+        Geometry(l, b);
+        Geometry((float)r);
+    }
+}
